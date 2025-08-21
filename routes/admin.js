b4008d4b0b0e5e2cfc6ac83c2e66c25b1b966212
Aman Kumar <aman140426@gmail.com>
2025-08-22 02:07:51 +0530

    write user routes code

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,35 @@
 const { Router } = require("express");
 
 const adminRouter = Router();
-const {adminModel}=require("../Schema/Schema")
+const { adminModel } = require("../Schema/Schema");
+const { z } = require("zod");
+const bcrypt = require("bcrypt");
+
 //adminRouter.use();
 
 adminRouter.post("/signup", function (req, res) {
+  const requiredBody = z.object({
+    email: z.string().email(),
+    password: z.string().max(10).min(6),
+    firstName: z.string().min(3),
+    secondName: z.string().min(3),
+  });
+
+  const validateData = requiredBody.safeParse(req.body);
+  if (!validateData.success) {
+    res.json({
+      msg: "invalid input",
+      error: validateData.error.issues,
+    });
+  }
+
+  const { email , password ,firstName, secondName}=validateData.data
+
+
+  // bcrypt
+
+  const hashPassword=bcrypt.hash(password,5);
+
   res.json({
     msg: "signup endpoint",
   });
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,104 @@
-const { Router } = require("express");
-
+const { Router, response } = require("express");
+const { z } = require("zod");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { userModel } = require("../Schema/Schema");
 const UserRouter = Router();
+require("dotenv").config();
+const JWT_USER_PASS = process.env.JWT_USER_PASS;
 
-UserRouter.post("/signup", function (req, res) {
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+
+UserRouter.post("/signup", async function (req, res) {
+  const requiredBody = z.object({
+    email: z.string().email(),
+    password: z.string(),
+    firstName: z.string().min(3),
+    secondName: z.string().min(3),
+  });
+
+  const validData = requiredBody.safeParse(req.body);
+  if (!validData.success) {
+    res.status(403).json({
+      error: validData.error.issues,
+    });
+  }
+
+  const { email, password, firstName, secondName } = validData.data;
+  // bcrypt password
+
+  try {
+    const hashPassword = await bcrypt.hash(password, 5);
+    await userModel.create({
+      email: email,
+      password: hashPassword,
+      firstName: firstName,
+      secondName: secondName,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
   res.json({
-    msg: "signup endpoint",
+    msg: "user signup",
   });
 });
 
-UserRouter.post("/login", function (req, res) {
-  res.json({
-    msg: "login endpoint",
+//**********************************************************************************************
+
+UserRouter.post("/login", async function (req, res) {
+  const requiredBody = z.object({
+    email: z.string().email(),
+    password: z.string(),
   });
+
+  const validateData = requiredBody.safeParse(req.body);
+
+  if (!validateData.success) {
+    res.status(403).json({
+      error: validateData.error.issues,
+    });
+    return 
+  }
+  const { email, password } = validateData.data;
+
+  const response = await userModel.findOne({
+    email: email,
+  });
+
+  if (!response) {
+    res.json({
+      msg: "user not found",
+    });
+    return 
+  }
+
+  const passwordMatch = await bcrypt.compare(password, response.password);
+
+  if (passwordMatch) {
+    const token = jwt.sign(
+      {
+        id: response._id.toString(),
+      },
+      JWT_USER_PASS
+    );
+
+    // cookie or session based logic
+
+    res.json({
+      token: token,
+    });
+  } else {
+    res.json({
+      msg: "incorrect credentials",
+    });
+  }
 });
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 UserRouter.get("/purchases", function (req, res) {
   res.json({
@@ -20,4 +106,4 @@ UserRouter.get("/purchases", function (req, res) {
   });
 });
 
-module.exports={ UserRouter: UserRouter };
+module.exports = { UserRouter: UserRouter };
